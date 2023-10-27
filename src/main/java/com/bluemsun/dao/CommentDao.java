213b721771f84caf51cbb373dd8b667ca8ac8fa6
GSteve-Paul <ljn1362623166@outlook.com>
2023-10-27 20:04:25 +0800

    忘记添加commentDao到git add

---
+++ src/main/java/com/bluemsun/dao/CommentDao.java
@@ -0,0 +1,24 @@
+package com.bluemsun.dao;
+
+import com.bluemsun.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentDao
+{
+    Integer insertComment(Comment comment);
+    Integer deleteComment(Long commentId);
+    Comment getComment(Long commentId);
+
+    //paramater comment means the upward
+    Integer getCommentAmount(Comment comment);
+
+    List<Comment> getCommentPage(Long blogId,Long commentId,Integer start,Integer len);
+    default List<Comment> getTheCommentPage(Comment comment,Integer start,Integer len){
+        return getCommentPage(comment.getBlogId(),comment.getCommentId()
+        ,start,len);
+    }
+
+}
